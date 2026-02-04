5ea7d8178f7dea6f2da726bdbe716829dd97014b
snehauppula <snehauppula23@gmail.com>
2026-02-04 19:56:37 +0530

    Refactor logger setup to create timestamped log files and ensure logs directory exists

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,15 @@
-import logger
+import logging
 import os
 from datetime import datetime
 
-LOG_FILE_PATH = f"logs/{datetime.now().strftime('%Y-%m-%d')}.log"
-logs_path= os.path.join(os.getcwd(),"logs",LOG_FILE_PATH)
+LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path= os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+log_file_path=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format='[%(asctime)s] %(lineno)d %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
