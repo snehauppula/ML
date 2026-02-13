ed64bb6d579c488045a938aaa1319f32126bd371
snehauppula <snehauppula23@gmail.com>
2026-02-14 00:12:25 +0530

    feat: Implement initial Flask web application for student exam performance prediction.

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,request,redirect
+import pandas as pd
+from src.pipeline.prediction import predictPipeline, CustomData
+from src.logger import logging
+from src.exception import CustomException
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        try:
+            gender = request.form['gender']
+            race_ethnicity = request.form['ethnicity']
+            parental_level_of_education = request.form['parental_level_of_education']
+            lunch = request.form['lunch']
+            test_preparation_course = request.form['test_preparation_course']
+            reading_score = request.form['reading_score']
+            writing_score = request.form['writing_score']
+            
+            data = CustomData(
+                gender = gender,
+                race_ethnicity = race_ethnicity,
+                parental_level_of_education = parental_level_of_education,
+                lunch = lunch,
+                test_preparation_course = test_preparation_course,
+                reading_score = reading_score,
+                writing_score = writing_score
+            )
+            data_df = data.get_data_as_data_frame()
+            data_df = StandardScaler().fit_transform(data_df)
+            pipeline = predictPipeline()
+            y_pred = pipeline.predict(data_df)
+            return render_template('index.html',results=y_pred)
+        except Exception as e:
+            raise CustomException(e, sys)
+   
+
+if __name__ == '__main__':
+    app.run(debug=True)
